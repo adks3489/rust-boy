89140bd3822ea79b5c9f5f81d299585e1215ddfa
James Chiang <adks3489@gmail.com>
2021-11-17 19:36:33 +0800

    feat: register

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    h: u8,
+    l: u8,
+}
+
+impl Registers {
+    // af, bc, de, hl
+    fn get_af(&self) -> u16 {
+        (self.a as u16) << 8 | self.f as u16
+    }
+    fn set_af(&mut self, value: u16) {
+        self.a = ((value & 0xFF00) >> 8) as u8;
+        self.f = (value & 0xFF) as u8;
+    }
+    fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8 | self.c as u16
+    }
+    fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+    fn get_de(&self) -> u16 {
+        (self.d as u16) << 8 | self.e as u16
+    }
+    fn set_de(&mut self, value: u16) {
+        self.d = ((value & 0xFF00) >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
+    }
+    fn get_hl(&self) -> u16 {
+        (self.h as u16) << 8 | self.h as u16
+    }
+    fn set_hl(&mut self, value: u16) {
+        self.h = ((value & 0xFF00) >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
+    }
+}
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+impl std::convert::From<FlagsRegister> for u8 {
+    fn from(flag: FlagsRegister) -> Self {
+        (if flag.zero { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION
+            | (if flag.subtract { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION
+            | (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION
+            | (if flag.carry { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        FlagsRegister {
+            zero: ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0,
+            subtract: ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0,
+            half_carry: ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0,
+            carry: ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0,
+        }
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
